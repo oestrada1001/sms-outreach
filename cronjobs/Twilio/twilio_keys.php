dc2b9ce1946d9a206e020965b8c4e4aaf54addce
Oscar Estrada <o.estrada1001@gmail.com>
2017-08-31 11:10:05 -0700

    Included Twilio test keys for testing. Live keys are commented out.

--- cronjobs/Twilio/twilio_keys.php
+++ cronjobs/Twilio/twilio_keys.php
@@ -1,6 +1,10 @@
 <?php
+//Live Keys
+//$sid = 'AC9aa8585446af8cafc529ba2f3bff7511';
+//$twilio_token = 'c7bc34c5cc067cae53726cfc22419cdf';
 
-$sid = 'AC9aa8585446af8cafc529ba2f3bff7511';
-$token = 'c7bc34c5cc067cae53726cfc22419cdf';
+//Test Keys
+$sid = 'AC5f7412f79a253c0daa26cfcfd99e1bbc';
+$twilio_token = '57db6f5dcb1bfbdbf471f97067c8f934';
 
 ?>
